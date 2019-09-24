55670645fbf42671cf8197ec568429c4da11aa61
Kamila Wickramarachchi <lakith7@berkeley.edu>
2019-09-23 21:33:57 -0700

    a

--- Mandelbrot.c
+++ Mandelbrot.c
@@ -20,7 +20,7 @@ u_int64_t MandelbrotIterations(u_int64_t maxiters, ComplexNumber * point, double
 	struct ComplexNumber *complexY;
 	struct ComplexNumber *complexX = newComplexNumber(0, 0);
 
-	while (maxiters >= 0) {
+	while (maxiters > 0) {
 		if (ComplexAbs(complexX) > threshold) {
 			freeComplexNumber(complexX);
 			return iters;
@@ -44,28 +44,33 @@ Scale is the the distance between center and the top pixel in one dimension.
 void Mandelbrot(double threshold, u_int64_t max_iterations, ComplexNumber* center, double scale, u_int64_t resolution, u_int64_t * output) {
 	double realCenter = Re(center);
 	double imCenter = Im(center);
-	double increments = scale/resolution;
-	double imaginary;
-	double real;
-
-	int length = (2 * resolution + 1);
-	int height = 0;
-	int width = 0;
-	int index = 0;
-	while (height < length) {
-		imaginary = imCenter + scale - (increments * height);
-		while (width < length) {
-			real = realCenter - scale + (increments * width);
-			ComplexNumber *newPoint = newComplexNumber(real, imaginary);
-			output[index] = MandelbrotIterations(max_iterations, newPoint, threshold);
-			freeComplexNumber(newPoint);
-			index += 1;
-			width += 1;
-		}
-		height += 1;
-		width = 0;
+	if (resolution = 0) {
+		output[0] = MandelbrotIterations(max_iterations, center, threshold);
 	}
-	freeComplexNumber(center);
+	else {
+		double increments = scale/resolution;
+		double imaginary;
+		double real;
+
+		int length = (2 * resolution + 1);
+		int height = 0;
+		int width = 0;
+		int index = 0;
+		while (height < length) {
+			imaginary = imCenter + scale - (increments * height);
+			while (width < length) {
+				real = realCenter - scale + (increments * width);
+				ComplexNumber *newPoint = newComplexNumber(real, imaginary);
+				output[index] = MandelbrotIterations(max_iterations, newPoint, threshold);
+				freeComplexNumber(newPoint);
+				index += 1;
+				width += 1;
+			}
+			height += 1;
+			width = 0;
+		}
+		freeComplexNumber(center);
+	}
 }
 
 
