e4f9263269afa702e2eee59b33dd07a88f7dca65
Kamila Wickramarachchi <lakith7@berkeley.edu>
2019-10-01 00:01:22 -0700

    adding

--- ColorMapInput.c
+++ ColorMapInput.c
@@ -15,9 +15,78 @@
 **This function reads in a file name colorfile.
 **It then uses the information in colorfile to create a color array, with each color represented by an int[3].
 ***************/
-uint8_t** FileToColorMap(char* colorfile, int* colorcount)
-{
-	//YOUR CODE HERE
+uint8_t** FileToColorMap(char* colorfile, int* colorcount) {
+	FILE* fileptr = fopen(colorfile, "r");
+
+	if (fileptr == NULL) {
+		fclose(fileptr);
+		printf("No such file.");
+		return 0;
+	}
+
+	fscanf(fileptr, "%u", colorcount);
+
+	uint8_t* storage1 = malloc(sizeof(uint8_t));
+	uint8_t* storage2 = malloc(sizeof(uint8_t));
+	uint8_t* storage3 = malloc(sizeof(uint8_t));
+
+	if ((*colorcount) <= 0) {
+		fclose(fileptr);
+		printf("colorcount argument is at or below zero");
+		return 0;
+	}
+
+	uint8_t** returnPointer = (uint8_t**) malloc((*colorcount) * sizeof(uint8_t*));
+	if (returnPointer == NULL) {
+		printf("memory allocation problems");
+		free(returnPointer);
+		return 0;
+	}
+	for (int i = 0; i < (*colorcount); i++) {
+		returnPointer[i] = (uint8_t*) malloc(3 * sizeof(uint8_t));
+		if (returnPointer[i] == NULL) {
+			printf("memory allocation problems");
+			while (i >= 0) {
+				free(returnPointer[i]);
+				i -= 1;
+			}
+			fclose(fileptr);
+			free(returnPointer);
+			return 0;
+		}
+	}
+	/* Do we have to malloc the null character or nah? */
+	int index = 0;
+	int numberColors = *colorcount;
+	printf("%d", numberColors);
+	while (numberColors > 0) {
+		if (fscanf(fileptr, "%u %u %u", storage1, storage2, storage3) != 3) {
+			printf("the input file has problematic formatting");
+			freeDoublePointer(returnPointer, colorcount);
+			return 0;
+		}
+		returnPointer[index][0] = *storage1;
+		returnPointer[index][1] = *storage2;
+		returnPointer[index][2] = *storage3;
+		/* Do we have to malloc the below commands or nah? */
+        numberColors -= 1;
+        index += 1;
+	}
+	free(storage1);
+	free(storage2);
+	free(storage3);
+    fclose(fileptr);
+    return returnPointer;
+}
+
+void freeDoublePointer(uint8_t** input, int* colorcount) {
+	int numberOfColors = *colorcount;
+	int index = 0;
+	while (index < numberOfColors) {
+		free(input[index]);
+		index += 1;
+	}
+	free(input);
 }
 
 
